49b32b8403aceae2eb85646c1c35b91f8c13ad4d
Iván Simarro <ivan.simarro.pernudo@gmail.com>
2022-03-22 18:33:58 +0100

    [full] Added selected to Navbar. Continuing with more info to the API

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -9,32 +9,32 @@ export default function Navbar() {
     return (
         <ul className="navbar">
             <li>
-                <Link className='navbar__item' to="/" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/" ? 'navbar__item selected' : 'navbar__item'} to="/" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <FaHome style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
             <li>
-                <Link className='navbar__item' to="/search" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/search" ? 'navbar__item selected' : 'navbar__item'} to="/search" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <FaSearch style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
             <li>
-                <Link className='navbar__item' to="/cart" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/cart" ? 'navbar__item selected' : 'navbar__item'} to="/cart" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <FaShoppingCart style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
             <li>
-                <Link className='navbar__item' to="/favourites" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/favourites" ? 'navbar__item selected' : 'navbar__item'} to="/favourites" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <FaHeart style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
             <li>
-                <Link className='navbar__item' to="/profile" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/profile" ? 'navbar__item selected' : 'navbar__item'} to="/profile" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <FaUserAlt style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
             <li>
-                <Link className='navbar__item' to="/contact" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
+                <Link className={location.pathname === "/contact" ? 'navbar__item selected' : 'navbar__item'} to="/contact" onClick={() => window.scrollTo({ top: 0, behavior: "smooth", })} >
                     <BsChatSquareDotsFill style={{ fontSize: "1.7rem" }} />
                 </Link>
             </li>
