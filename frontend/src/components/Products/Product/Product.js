76cd600f10e658f4725d3919c1c91a16d8f4c639
Iván Simarro <ivan.simarro.pernudo@gmail.com>
2022-03-23 16:26:54 +0100

    [frontend] Fixed non-stock style

--- frontend/src/components/Products/Product/Product.js
+++ frontend/src/components/Products/Product/Product.js
@@ -3,7 +3,7 @@ import "./Product.scss";
 export default function Product({ name, image, description, price, brand, stock, type, quantity }) {
     return (
         <li className="product">
-            <img className={stock ? "product__img" : "product__img--nostock"} src={image} alt="" />
+            <img className={stock ? "product__img" : "product__img product__img--nostock"} src={image} alt="" />
             <div className="product__data">
                 <h1 className="product__data--price">{`${price} €`}</h1>
                 <h2 className="product__data--title">{name + " " + quantity}</h2>
