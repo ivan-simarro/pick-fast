0a5abe4c70e23145e5a1d24227806df61ff69d50
Iván Simarro <ivan.simarro.pernudo@gmail.com>
2022-03-24 23:26:26 +0100

    [frontend] Adding animation to products and editing styles

--- frontend/src/components/Products/Product/Product.js
+++ frontend/src/components/Products/Product/Product.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 function Product({ name, image, description, price, brand, stock, type, quantity }) {
     const [toCart, setToCart] = useState(1);
     const [isAdded, setIsAdded] = useState(false);
+    const [show, setShow] = useState(sessionStorage.getItem("productAnimation") || false);
 
     useEffect(() => {
         toCart <= 0 && setToCart(1);
@@ -13,10 +14,16 @@ function Product({ name, image, description, price, brand, stock, type, quantity
     function handleAddToCart(e, name) {
         setIsAdded(true);
     }
+    if (!show) {
+        setTimeout(() => {
+            setShow(true);
+            sessionStorage.setItem("productAnimation", true);
+        }, 1500);
+    }
 
 
     return (
-        <li className="product">
+        <li className={!show ? "product short" : "product"}>
             {
                 isAdded && <img className="product__cart" src={carrito} alt="Added to cart" />
             }
@@ -24,15 +31,16 @@ function Product({ name, image, description, price, brand, stock, type, quantity
                 <img className={stock ? "product__img" : "product__img product__img--nostock"} src={image} alt="" />
             </div>
             <div className="product__data">
-                <h1 className="product__data--price">{`${price} €`}</h1>
-                <h2 className="product__data--title">{name + " " + quantity}</h2>
+                {show && <>
+                    <h1 className="product__data--price">{`${price} €`}</h1>
+                    <h2 className="product__data--title">{name + " " + quantity}</h2>
+                </>
+                }
                 <div className="product__data--add">
                     {
                         stock ?
                             <>
-                                <button className="product__data--add-number" onClick={() => setToCart(toCart => toCart - 1)}>-</button>
-                                <input type="number" className="product__data--add-number" size="3" value={toCart} min="1" pattern="^[0-9]+" max="100" onChange={(e) => setToCart(e.target.value)}></input>
-                                <button className="product__data--add-number" onClick={() => setToCart(toCart => toCart + 1)}>+</button>
+                                {show && <><button className="product__data--add-number" onClick={() => setToCart(toCart => toCart - 1)}>-</button><input type="number" className="product__data--add-number" size="3" value={toCart} min="1" pattern="^[0-9]+" max="100" onChange={(e) => setToCart(e.target.value)}></input><button className="product__data--add-number" onClick={() => setToCart(toCart => toCart + 1)}>+</button></>}
                                 <div className="product__data--add-btn">
                                     <button style={{ cursor: "pointer" }} onClick={(e) => handleAddToCart(e, name)}>Añadir al carrito</button>
                                 </div></>
--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -7,10 +7,16 @@ export default function Products() {
 
     const [products, setProducts, loading, setLoading] = useOutletContext();
 
+    if (products.length === 0) {
+        sessionStorage.removeItem("productAnimation")
+    }
+
     return (
         <ul className="products">
             {
-                !loading ? products.map(product => <Product key={product.id} {...product} />) : <Spinner style={{ fontSize: "20rem", color: "white" }} />
+                !loading
+                    ? products.map(product => <Product key={product.id} {...product} />)
+                    : <Spinner style={{ fontSize: "8rem", color: "white", position: "absolute", top: "0", bottom: "0", right: "0", left: "0", margin: "auto" }} />
             }
         </ul>
     )
