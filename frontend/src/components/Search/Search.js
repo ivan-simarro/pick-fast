6658a206b5097e6795a0581c439eb791830bc452
Iván Simarro <ivan.simarro.pernudo@gmail.com>
2022-04-20 21:51:20 +0200

    [frontend] Styling favourites

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,9 +1,14 @@
 import "./Search.scss";
+import { useLocation } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
 
 export default function Search({ setSearchTerm, searchTerm }) {
 
+    const location = useLocation().pathname;
+
     return (
         <div className="search">
+            {location === "/favourites" && <p className="search__text">Aquí están tus productos favoritos <FaHeart style={{ color: "red", fontSize: "1.2rem" }} /></p>}
             <p>¿Que producto estás buscando?</p>
             <input className="search__input" placeholder="Search..." type="text" name="searchTerm" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
         </div>
