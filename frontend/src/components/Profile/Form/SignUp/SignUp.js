ae346b1ae3b0d681260e8dec39d90185b3c886fb
Iván <ivan.simarro.pernudo@gmail.com>
2022-05-08 16:12:14 +0200

    [fullstack] Trying to do post method, with error 403 forbidden

--- frontend/src/components/Profile/Form/SignUp/SignUp.js
+++ frontend/src/components/Profile/Form/SignUp/SignUp.js
@@ -1,24 +1,74 @@
+import axios from 'axios';
+import { useState } from 'react';
+import md5 from 'js-md5';
+import Swal from 'sweetalert2';
+
+const ENDPOINT_USERS = 'users/users/';
+
 export default function SignUp({ setLogged }) {
+    const [user, setUser] = useState({
+        name: '',
+        lastname: '',
+        user: '',
+        password: ''
+    })
+
+    function onChangeSignUp(e) {
+        setUser({ ...user, [e.target.name]: e.target.name === 'password' ? md5(e.target.value) : e.target.value });
+    }
+
+    function signup() {
+        if (!user.user.includes('@') || !user.user.includes('.')) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Email incorrecto',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else if (user.password === md5('') || user.password.length === 0) {
+            Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Contraseña inválida',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else {
+            axios.post(ENDPOINT_USERS, user)
+                .then(response => setLogged(true))
+                .catch(error => {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Ocurrió un error en su registro',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                });
+        }
+    }
+
     return (
         <div className="signup" >
             <h1 className="form__h1">Registrarse</h1>
             <form action="/" method="post">
                 <div className="top-row">
                     <div className="field-wrap">
-                        <input type="text" maxLength={15} placeholder="Nombre" required />
+                        <input type="text" name='name' onChange={onChangeSignUp} maxLength={15} placeholder="Nombre" required />
                     </div>
                     <div className="field-wrap" >
-                        <input type="text" maxLength={20} placeholder="Apellido" required />
+                        <input type="text" name='lastname' onChange={onChangeSignUp} maxLength={20} placeholder="Apellido" required />
                     </div >
                 </div >
                 <div className="field-wrap" >
-                    <input type="email" placeholder="Correo electrónico" required />
+                    <input type="email" name='user' onChange={onChangeSignUp} placeholder="Correo electrónico" required />
                 </div >
                 <div className="field-wrap" >
-                    <input type="password" maxLength={20} placeholder="Contraseña" required />
+                    <input type="password" name='password' onChange={onChangeSignUp} maxLength={20} placeholder="Contraseña" required />
                 </div>
             </form >
-            <button className="form__button button-block" onClick={() => setLogged(true)}>Empezar</button >
+            <button className="form__button button-block" onClick={signup}>Empezar</button >
         </div >
     )
 }
